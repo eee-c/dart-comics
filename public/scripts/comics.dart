251c8d0bf85b4f14f270849d1923acd217d704ad
Chris Strom <eee.c@eeecooks.com>
2012-01-21 00:18:44 -0500

    animate-frames

--- public/scripts/comics.dart
+++ public/scripts/comics.dart
@@ -17,8 +17,16 @@ attach_add_handler() {
 enable_add_form(event) {
   final form_div = document.query('#add-comic-form');
 
-  form_div.classes.remove('fade-out');
-  form_div.classes.add('fade-in');
+  var start = new Date.now();
+  step(timestamp) {
+    var progress = timestamp - start.value
+      , opacity = "${progress/1000}";
+    print(opacity);
+    form_div.style.opacity = opacity;
+    if (progress < 1000) window.webkitRequestAnimationFrame(step, form_div);
+    true;
+  }
+  window.webkitRequestAnimationFrame(step, form_div);
 
   form_div.queryAll('a').forEach((el) {
     el.on.click.add(disable_add_form);
