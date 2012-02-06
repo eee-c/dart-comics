9ebceac8885aebd99fd94592da3ed6bc7b141530
Chris Strom <eee.c@eeecooks.com>
2012-02-05 22:55:38 -0500

    FIXME! This should go away when the dart folks resolve the issues surrounding importing and prefixes.

--- public/scripts/main.dart
+++ public/scripts/main.dart
@@ -1,16 +1,16 @@
 #import('Collections.Comics.dart', prefix: 'Collections');
-#import('Views.Comics.dart', prefix: 'Views');
-#import('Views.AddComic.dart', prefix: 'Views');
+#import('Views.Comics.dart', prefix: 'ViewsFIXME01');
+#import('Views.AddComic.dart', prefix: 'ViewsFIXME02');
 
 main() {
 
   var my_comics_collection = new Collections.Comics()
-    , comics_view = new Views.Comics(
+    , comics_view = new ViewsFIXME01.Comics(
         el:'#comics-list',
         collection: my_comics_collection
       );
 
   my_comics_collection.fetch();
 
-  new Views.AddComic(el:'#add-comic');
+  new ViewsFIXME02.AddComic(el:'#add-comic');
 }
