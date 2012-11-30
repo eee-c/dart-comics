4f23e6a86dfd712b5246591b2d087c05ec6d89c9
Chris Strom <eee.c@eeecooks.com>
2012-11-29 19:57:00 -0500

    Going nuts with cascades

--- public/scripts/web/ModalDialog.dart
+++ public/scripts/web/ModalDialog.dart
@@ -47,46 +47,41 @@ class ModalDialog {
   Element query(String selectors) => el.query(selectors);
 
   _attachHanders() {
-    window.
-      on.
-      keyDown.
-      add((event) {
-        if (event.keyCode == 27) remove();
-      });
-
-    window.
-      on.
-      resize.
-      add(resizeHandler);
+    window.on
+      ..keyDown.
+          add((event) {
+            if (event.keyCode == 27) remove();
+          })
+      ..resize.
+          add(resizeHandler);
   }
 
   _removeHandlers() {
-    window.
-      on.
+    window.on.
       resize.
-      remove(resizeHandler);
+        remove(resizeHandler);
   }
 
   _drawBackground([event]) {
-    print('_drawBackground');
     if (bg != null) return;
 
-    bg = new Element.tag('div');
+    bg = new DivElement();
     document.body.nodes.add(bg);
 
     window.requestLayoutFrame(() {
       var doc = window.document.documentElement;
 
-      bg.style.position = 'absolute';
-      bg.style.top = '0px';
-      bg.style.left = '0px';
+      bg.style
+        ..position = 'absolute'
+        ..top = '0px'
+        ..left = '0px'
 
-      bg.style.width = "${doc.offsetWidth}px";
-      bg.style.height = "${doc.clientHeight}px";
+        ..width = "${doc.offsetWidth}px"
+        ..height = "${doc.clientHeight}px"
 
-      bg.style.backgroundColor = 'black';
-      bg.style.opacity = '0.8';
-      bg.style.zIndex = '1000';
+        ..backgroundColor = 'black'
+        ..opacity = '0.8'
+        ..zIndex = '1000';
     });
   }
 
@@ -96,24 +91,27 @@ class ModalDialog {
     window.requestLayoutFrame(() {
       var doc = window.document.documentElement;
 
-      el.style.opacity = '0';
+      el.style
+        ..opacity = '0'
 
-      el.style.position = 'absolute';
-      el.style.top = '0px';
-      el.style.left = '0px';
+        ..position = 'absolute'
+        ..top = '0px'
+        ..left = '0px'
 
-      el.style.backgroundColor = 'white';
-      el.style.padding = '20px';
-      el.style.borderRadius = '10px';
+        ..backgroundColor = 'white'
+        ..padding = '20px'
+        ..borderRadius = '10px'
 
-      el.style.zIndex = '1001';
+        ..zIndex = '1001'
 
-      el.style.top = '80px';
+        ..top = '80px'
+
+        ..transition = 'opacity 1s ease-in-out'
+        ..opacity = '1';
+
       int offset_left = (doc.offsetWidth/2 - el.offsetWidth/2).toInt();
       el.style.left = "${offset_left}px";
 
-      el.style.transition = 'opacity 1s ease-in-out';
-      el.style.opacity = '1';
     });
   }
 }
