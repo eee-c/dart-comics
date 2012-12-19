9c53a413fdbb925f7407f1a11c9e984185c93e79
Chris Strom <eee.c@eeecooks.com>
2012-12-18 20:19:40 -0500

    Some M1 fixes in localSync

---
+++ app/lib/main.dart
@@ -0,0 +1,58 @@
+library app;
+
+import 'package:scripts/Collections.Comics.dart' as Collections;
+import 'package:scripts/Views.Comics.dart' as Views;
+import 'package:scripts/Views.AddComic.dart' as Views;
+
+import 'dart:html';
+import 'dart:json';
+
+import 'package:hipster_mvc/hipster_sync.dart';
+
+main() {
+  // HipsterSync.sync = localSync;
+
+  var my_comics_collection = new Collections.Comics()
+    , comics_view = new Views.Comics(
+        el:'#comics-list',
+        collection: my_comics_collection
+      );
+
+  my_comics_collection.fetch();
+
+  new Views.AddComic(
+    el:'#add-comic',
+    collection: my_comics_collection
+  );
+}
+
+localSync(method, model) {
+  final completer = new Completer();
+
+  print("[local_sync] $method");
+
+  if (method == 'read') {
+    var json =  window.localStorage[model.url],
+        data = (json == null) ? {} : JSON.parse(json);
+
+    completer.complete(data.values);
+  }
+
+  if (method == 'create') {
+    var collection = model.collection,
+        attrs = model.attributes;
+
+    if (attrs['id'] == null) {
+      attrs['id'] = "${attrs['title']}:::${attrs['author']}".hashCode();
+    }
+    print(attrs);
+
+    collection.create(attrs);
+
+    var id = attrs['id'];
+    collection.data[id] = attrs;
+    window.localStorage.setItem(collection.url, JSON.stringify(collection.data));
+  }
+
+  return completer.future;
+}
